6ad0b9bdd3e6449e3e1e115371dcfd318830b231
Neeraja <neerajareddy24@gmail.com>
2018-09-16 15:34:21 -0400

    updated portfolio.html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("#sidebarCollapse").on("click", function() {
     $("#sidebar").toggleClass("active");
+    $("#content").toggleClass("active");
     $(this).toggleClass("fa-angle-double-right fa-angle-double-left");
   });
 });
